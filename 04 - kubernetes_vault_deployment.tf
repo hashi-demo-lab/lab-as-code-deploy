c9733c684ff9627c6fff16fe847a72fe4186e2c8
Aaron Evans <aarone@outlook.com.au>
2024-09-30 20:33:03 +1000

    Add Kubernetes role and job for Vault initialization and unsealing

--- 04 - kubernetes_vault_deployment.tf
+++ 04 - kubernetes_vault_deployment.tf
@@ -1,3 +1,35 @@
+resource "kubernetes_role_v1" "vault_init_role" {
+  metadata {
+    name      = "vault-init-role"
+    namespace = kubernetes_namespace.vault.id
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["secrets"]
+    verbs      = ["create", "get", "update"]
+  }
+}
+
+resource "kubernetes_role_binding_v1" "vault_init_role_binding" {
+  metadata {
+    name      = "vault-init-role-binding"
+    namespace = kubernetes_namespace.vault.id
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role_v1.vault_init_role.metadata[0].name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "default"
+    namespace = kubernetes_namespace.vault.id
+  }
+}
+
 resource "helm_release" "ingress_nginx" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
@@ -55,6 +87,7 @@ resource "kubernetes_job_v1" "vault_init" {
           name  = "vault-init"
           image = "hashicorp/vault:latest" # Same version as Vault Helm chart
 
+
           command = [
             "/bin/sh",
             "-c",
@@ -63,12 +96,13 @@ resource "kubernetes_job_v1" "vault_init" {
             echo "Initializing Vault..."
             
             # Install jq using apk, which is used in Alpine-based images
-            apk --no-cache add curl jq
+            apk --no-cache add curl jq kubectl
 
             export VAULT_CACERT=/vault/tls/ca.crt  # Path to the CA certificate
             export VAULT_ADDR=https://vault-0.vault-internal.vault.svc.cluster.local:8200
             init_output=$(vault operator init -key-shares=1 -key-threshold=1 -format=json)
             root_token=$(echo $init_output | jq -r '.root_token')
+
             unseal_key=$(echo $init_output | jq -r '.unseal_keys_b64[0]')
             
             echo "Root token: $root_token"
@@ -79,6 +113,10 @@ resource "kubernetes_job_v1" "vault_init" {
             LEADER_CA_CERT=$(cat /vault/tls/ca.crt)
             echo "Unsealing Vault pod..."
             vault operator unseal $unseal_key
+
+            export VAULT_TOKEN=$root_token
+            echo "Enabling Vault audit logging..."
+            vault audit enable file file_path=/vault/audit/vault_audit.log
 
             # Join and unseal vault-1
             export VAULT_ADDR=https://vault-1.vault-internal.vault.svc.cluster.local:8200
@@ -93,6 +131,11 @@ resource "kubernetes_job_v1" "vault_init" {
             vault operator unseal $unseal_key
 
             echo "Vault initialization and unsealing complete!"
+
+            kubectl create secret generic vault-init-credentials \
+              --from-literal=root-token=$root_token \
+              --from-literal=unseal-key=$unseal_key \
+              -n ${kubernetes_namespace.vault.id}
             EOT
           ]
 
