e5d33c7055bc8aaf6c7ff4392fb9927ab31101df
Aaron Evans <aarone@outlook.com.au>
2024-10-10 23:19:27 +1100

    Refactor Vault deployment to use dynamic namespace references and improve resource definitions

--- 06 - vault_deployment.tf
+++ 06 - vault_deployment.tf
@@ -2,18 +2,19 @@
 resource "kubernetes_secret_v1" "vault_license" {
   metadata {
     name      = "vaultlicense"
-    namespace = kubernetes_namespace.vault.id
+    namespace = kubernetes_namespace.namespace["vault"].id
   }
   data = {
     license = var.vault_license
   }
-  type = "kubernetes.io/opaque"
+  type = "Opaque" # Corrected to Opaque, as kubernetes.io/opaque is not valid
 }
 
+# Role for Vault initialization to manage secrets
 resource "kubernetes_role_v1" "vault_init_role" {
   metadata {
     name      = "vault-init-role"
-    namespace = kubernetes_namespace.vault.id
+    namespace = kubernetes_namespace.namespace["vault"].id
   }
 
   rule {
@@ -23,10 +24,11 @@ resource "kubernetes_role_v1" "vault_init_role" {
   }
 }
 
+# Binding role to default service account for Vault init
 resource "kubernetes_role_binding_v1" "vault_init_role_binding" {
   metadata {
     name      = "vault-init-role-binding"
-    namespace = kubernetes_namespace.vault.id
+    namespace = kubernetes_namespace.namespace["vault"].id
   }
 
   role_ref {
@@ -38,10 +40,11 @@ resource "kubernetes_role_binding_v1" "vault_init_role_binding" {
   subject {
     kind      = "ServiceAccount"
     name      = "default"
-    namespace = kubernetes_namespace.vault.id
+    namespace = kubernetes_namespace.namespace["vault"].id
   }
 }
 
+# Helm release for Vault
 resource "helm_release" "vault" {
   depends_on = [
     kubernetes_secret_v1.vault_license,
@@ -51,7 +54,7 @@ resource "helm_release" "vault" {
   name       = "vault"
   repository = "https://helm.releases.hashicorp.com"
   chart      = "vault"
-  namespace  = kubernetes_namespace.vault.id
+  namespace  = kubernetes_namespace.namespace["vault"].id
   version    = var.vault_helm_version
 
   values = [
@@ -67,14 +70,33 @@ resource "helm_release" "vault" {
     name  = "server.ingress.tls[0].hosts[0]"
     value = "vault-dc1.hashibank.com"
   }
+  
+  lifecycle {
+    ignore_changes = [
+      metadata,  # Ignores all changes within the metadata block
+    ]
+  }
 }
 
+# ConfigMap to store the Vault init script
+resource "kubernetes_config_map_v1" "vault_init_script" {
+  metadata {
+    name      = "vault-init-script"
+    namespace = kubernetes_namespace.namespace["vault"].id
+  }
+
+  data = {
+    "vault-init.sh" = local.vault_init_script # Reference the script loaded via locals
+  }
+}
+
+# Kubernetes job to initialize Vault
 resource "kubernetes_job_v1" "vault_init" {
-  depends_on = [helm_release.vault] # Add a dependency on the Helm release
+  depends_on = [helm_release.vault]
 
   metadata {
     name      = "vault-init-job"
-    namespace = kubernetes_namespace.vault.id
+    namespace = kubernetes_namespace.namespace["vault"].id
   }
 
   spec {
@@ -90,59 +112,22 @@ resource "kubernetes_job_v1" "vault_init" {
 
         container {
           name  = "vault-init"
-          image = "hashicorp/vault:latest" # Same version as Vault Helm chart
+          image = "hashicorp/vault:latest"
 
+          # Use the script from the ConfigMap
+          command = ["/bin/sh", "/vault-init/vault-init.sh"]
 
-          command = [
-            "/bin/sh",
-            "-c",
-            <<EOT
+          # Pass the Kubernetes namespace as an environment variable
+          env {
+            name  = "K8S_NAMESPACE"
+            value = kubernetes_namespace.namespace["vault"].id
+          }
 
-            echo "Initializing Vault..."
-            
-            # Install jq using apk, which is used in Alpine-based images
-            apk --no-cache add curl jq kubectl
-
-            export VAULT_CACERT=/vault/tls/ca.crt  # Path to the CA certificate
-            export VAULT_ADDR=https://vault-0.vault-internal.vault.svc.cluster.local:8200
-            init_output=$(vault operator init -key-shares=1 -key-threshold=1 -format=json)
-            root_token=$(echo $init_output | jq -r '.root_token')
-
-            unseal_key=$(echo $init_output | jq -r '.unseal_keys_b64[0]')
-            
-            echo "Root token: $root_token"
-            echo "Unseal key: $unseal_key"
-
-            # Unseal the same pod we just initialized (self-unsealing)
-            # Load the CA certificate content (not the path)
-            LEADER_CA_CERT=$(cat /vault/tls/ca.crt)
-            echo "Unsealing Vault pod..."
-            vault operator unseal $unseal_key
-
-            export VAULT_TOKEN=$root_token
-            echo "Enabling Vault audit logging..."
-            vault audit enable file file_path=/vault/audit/vault_audit.log
-
-            # Join and unseal vault-1
-            export VAULT_ADDR=https://vault-1.vault-internal.vault.svc.cluster.local:8200
-            echo "Joining and unsealing vault-1..."
-            vault operator raft join -leader-ca-cert="$(cat /vault/tls/ca.crt)" https://vault-0.vault-internal.vault.svc.cluster.local:8200
-            vault operator unseal $unseal_key
-
-            # Join and unseal vault-2
-            export VAULT_ADDR=https://vault-2.vault-internal.vault.svc.cluster.local:8200
-            echo "Joining and unsealing vault-2..."
-            vault operator raft join -leader-ca-cert="$(cat /vault/tls/ca.crt)" https://vault-0.vault-internal.vault.svc.cluster.local:8200
-            vault operator unseal $unseal_key
-
-            echo "Vault initialization and unsealing complete!"
-
-            kubectl create secret generic vault-init-credentials \
-              --from-literal=root-token=$root_token \
-              --from-literal=unseal-key=$unseal_key \
-              -n ${kubernetes_namespace.vault.id}
-            EOT
-          ]
+          volume_mount {
+            name       = "vault-init-script"
+            mount_path = "/vault-init"
+            read_only  = true
+          }
 
           volume_mount {
             name       = "vault-tls"
@@ -150,13 +135,26 @@ resource "kubernetes_job_v1" "vault_init" {
             read_only  = true
           }
         }
+
+        volume {
+          name = "vault-init-script"
+
+          config_map {
+            name = "vault-init-script"
+            items {
+              key  = "vault-init.sh"
+              path = "vault-init.sh"
+            }
+          }
+        }
+
         volume {
           name = "vault-tls"
 
           secret {
-            secret_name = "vaultcertificate" # Kubernetes secret that contains your TLS certs
+            secret_name = "vault-certificate"
             items {
-              key  = "ca.crt" # Only mount the CA certificate
+              key  = "ca.crt"
               path = "ca.crt"
             }
           }
@@ -166,12 +164,12 @@ resource "kubernetes_job_v1" "vault_init" {
   }
 }
 
-# Fetch the Vault root token and unseal key from the Kubernetes secret
+# Fetch the Vault root token and unseal key from Kubernetes secret
 data "kubernetes_secret" "vault_init_credentials" {
   metadata {
     name      = "vault-init-credentials"
-    namespace = kubernetes_namespace.vault.id
+    namespace = kubernetes_namespace.namespace["vault"].id
   }
 
-  depends_on = [kubernetes_job_v1.vault_init] # Add a dependency on the job
+  depends_on = [kubernetes_job_v1.vault_init] # Ensure the job runs before accessing secret
 }
