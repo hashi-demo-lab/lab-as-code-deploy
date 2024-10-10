7d5bff953505c697ae6d1a9dc577e9091b0636c2
Aaron Evans <aarone@outlook.com.au>
2024-10-10 23:19:12 +1100

    Update Ingress NGINX deployment to use dynamic namespace reference and enable SSL passthrough

--- 05 - ingress_nginx_deployment.tf
+++ 05 - ingress_nginx_deployment.tf
@@ -1,16 +1,18 @@
+# Helm release for Ingress NGINX Controller
 resource "helm_release" "ingress_nginx" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  namespace  = kubernetes_namespace.nginx.id
+  namespace  = kubernetes_namespace.namespace["nginx"].id
 
+  # Setting values to configure the NGINX controller
   set {
     name  = "controller.watchNamespace" # Ensure it watches all namespaces
-    value = ""
+    value = ""                          # Empty value means no restriction (all namespaces)
   }
 
   set {
-    name  = "controller.extraArgs.enable-ssl-passthrough"
-    value = ""
+    name  = "controller.extraArgs.enable-ssl-passthrough" # Enable SSL passthrough
+    value = "true"
   }
 }
