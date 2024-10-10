f7f074c4a3945dd9eded03ff65dbf5e675682113
Aaron Evans <aarone@outlook.com.au>
2024-10-11 10:44:43 +1100

    Update CA certificate configuration to change organization name and clean up formatting

--- 01 - pre_reqs/certificate_authority.tf
+++ 01 - pre_reqs/certificate_authority.tf
@@ -1,16 +1,14 @@
 provider "tls" {}
 
-# Step 1: Create the CA private key
 resource "tls_private_key" "ca_tls_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-# Step 2: Create the CA self-signed certificate
 resource "tls_self_signed_cert" "ca_cert" {
   private_key_pem = tls_private_key.ca_tls_key.private_key_pem
-  validity_period_hours = 87600 # CA certificate valid for 10 years
-  early_renewal_hours   = 720   # Renew 30 days before expiration
+  validity_period_hours = 87600
+  early_renewal_hours   = 720
   is_ca_certificate = true
 
   allowed_uses = [
@@ -21,11 +19,10 @@ resource "tls_self_signed_cert" "ca_cert" {
 
   subject {
     common_name  = "vault-ca"
-    organization = "HashiCorp"
+    organization = "lab-as-code"
   }
 }
 
-# Output the CA certificate and private key as files
 resource "local_file" "ca_cert" {
   filename = "${path.module}/ca_cert.pem"
   content  = tls_self_signed_cert.ca_cert.cert_pem
