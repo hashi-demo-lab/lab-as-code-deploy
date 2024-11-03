e7e590fbae0c2fdeb515097d8fc08d377a896cd5
Aaron Evans <aarone@outlook.com.au>
2024-11-04 03:00:27 +1100

    Remove commented-out OpenLDAP manifest paths from locals.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,3 @@
-# Define local variables for various file paths and sensitive data used in the configuration
 locals {
   # CA Private Key and Certificate used for signing Vault certificates
   ca_private_key_pem = module.ca_cert.key_file_path
@@ -15,13 +14,6 @@ locals {
   phpldapadmin_service = file("${path.root}/_manifests/openldap/phpldapadmin_service.yaml")
   openldap_ingress     = file("${path.root}/_manifests/openldap/ingress.yaml")
   ldap_ldif_data       = file("${path.root}/_manifests/openldap/hashibank.ldif")
-  # openldap_statefulset = file("./_manifests/openldap/statefulset.yaml")          # OpenLDAP StatefulSet manifest
-  # openldap_service     = file("./_manifests/openldap/ldap_service.yaml")         # OpenLDAP Service manifest
-  # phpldapadmin_service = file("./_manifests/openldap/phpldapadmin_service.yaml") # phpLDAPadmin Service manifest
-  # openldap_ingress     = file("./_manifests/openldap/ingress.yaml")              # Ingress for OpenLDAP and phpLDAPadmin
-  # hashibank_ldif       = file("./_manifests/openldap/hashibank.ldif")            # LDIF file for initializing LDAP with data
-  # Load OpenLDAP YAML manifests for resources
-
 
   # Prometheus Helm chart and ConfigMap for monitoring
   prometheus_scrape_config = file("./_helm_charts/prometheus/configmap.yaml")         # Prometheus ConfigMap for scrape configs
