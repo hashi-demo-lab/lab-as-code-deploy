e6d0b66b151920b5f282ec72a7fcb39a2e6fbe10
Simon Lynch <simon.lynch@hashicorp.com>
2024-11-04 11:50:28 +1100

    Add commands for managing certificates and updating /etc/hosts in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,5 +8,17 @@ output "vault_ca_cert" {
 
 output "https_ldap_common_name" {
     value = var.ldap_common_name
-  
+}
+
+# output "cert_full_path" {
+#     value = "${path.root}/${module.ca_cert.cert_file_path}"
+# }
+
+output "command_trust_cert" {
+    value = "sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ${path.root}/${module.ca_cert.cert_file_path}"
+}
+
+output "command_update_etc_hosts" {
+    #update /etc/hosts
+    value = "sudo echo \"${var.vault_common_name} 127.0.0.1\" >> /etc/hosts \nsudo echo \"${var.ldap_common_name} 127.0.0.1\" >> /etc/hosts"
 }
