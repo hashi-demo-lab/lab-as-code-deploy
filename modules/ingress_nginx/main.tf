dddd36f263c6c95b9f1abe443ff1973056e5465c
Aaron Evans <aarone@outlook.com.au>
2025-01-30 04:09:24 +1100

    Set SSL passthrough to true for NGINX ingress controller

--- modules/ingress_nginx/main.tf
+++ modules/ingress_nginx/main.tf
@@ -4,14 +4,8 @@ resource "helm_release" "ingress_nginx" {
   chart      = var.helm_chart_name
   namespace  = var.ingress_namespace  # Set dynamically from input variable
 
-  # Configuration for the NGINX controller
-  # set {
-  #   name  = "controller.watchNamespace"
-  #   value = var.controller_watch_namespace
-  # }
-
   set {
     name  = "controller.extraArgs.enable-ssl-passthrough"
-    value = var.enable_ssl_passthrough ? "true" : "false"
+    value = true
   }
 }
