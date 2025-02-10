07e8e8e064b5c55244dd0d0cb74dd1c9442046b3
Aaron Evans <aarone@outlook.com.au>
2025-02-10 14:49:03 +1100

    Add encoded CA certificate output to Vault configuration

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,7 @@ output "instructions" {
     "vault" = {
       "address" = "https://${var.vault_common_name}:443"
       "ca_cert" = module.ca_cert.cert_file_path
+      "encoded_ca_cert" = base64encode(module.ca_cert.cert_pem)
       "commands" = {
         "trust_cert"       = "sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ${path.root}/${module.ca_cert.cert_file_path}"
         "root_token"       = "kubectl config use-context docker-desktop; kubectl get secret vault-init-credentials -n vault -o jsonpath={.data.root-token} | base64 --decode"
