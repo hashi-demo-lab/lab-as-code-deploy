a01312377df7607df35e5ca115d869da6bea19d0
Aaron Evans <aarone@outlook.com.au>
2025-02-10 14:48:15 +1100

    Fix formatting in Helm release configuration for ingress_nginx

--- modules/ingress_nginx/main.tf
+++ modules/ingress_nginx/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "ingress_nginx" {
   name       = var.helm_release_name
   repository = var.helm_repository
   chart      = var.helm_chart_name
-  namespace  = var.ingress_namespace  # Set dynamically from input variable
+  namespace  = var.ingress_namespace # Set dynamically from input variable
 
   set {
     name  = "controller.extraArgs.enable-ssl-passthrough"
