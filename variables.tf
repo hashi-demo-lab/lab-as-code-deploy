55754c9b64e61922956cadbb66b2a24ae0d60224
Aaron Evans <aarone@outlook.com.au>
2025-02-15 14:51:48 -0600

    Update Vault variable configurations for primary and auto-unseal setups

--- variables.tf
+++ variables.tf
@@ -15,29 +15,48 @@ variable "vault_license" {
   description = "Vault License"
 }
 
-variable "vault_dns_names" {
+variable "primary_vault_common_name" {
+  type        = string
+  description = "Common name for Vault certificate"
+  default     = "vault.hashibank.com"
+}
+
+variable "primary_vault_dns_names" {
   type        = list(string)
   description = "DNS names for Vault certificate"
   default = [
-    "vault.vault.svc.cluster.local",
-    "vault-active.vault.svc.cluster.local",
-    "vault-standby.vault.svc.cluster.local",
-    "vault-0.vault.svc.cluster.local",
-    "vault-1.vault.svc.cluster.local",
-    "vault-2.vault.svc.cluster.local",
-    "vault-0.vault-internal.vault.svc.cluster.local",
-    "vault-1.vault-internal.vault.svc.cluster.local",
-    "vault-2.vault-internal.vault.svc.cluster.local",
-    "vault-dc1.hashibank.com",
-    "vault-active.hashibank.com",
-    "vault-standby.hashibank.com"
+    "vault.hashibank.com",
+    "vault.primary-vault.svc.cluster.local",
+    "vault-active.primary-vault.svc.cluster.local",
+    "vault-standby.primary-vault.svc.cluster.local",
+    "vault-internal.primary-vault.svc.cluster.local",
+    "vault-0.primary-vault.svc.cluster.local",
+    "vault-1.primary-vault.svc.cluster.local",
+    "vault-2.primary-vault.svc.cluster.local",
+    "vault-3.primary-vault.svc.cluster.local",
+    "vault-4.primary-vault.svc.cluster.local",
+    "vault-0.vault-internal.primary-vault.svc.cluster.local",
+    "vault-1.vault-internal.primary-vault.svc.cluster.local",
+    "vault-2.vault-internal.primary-vault.svc.cluster.local",
+    "vault-3.vault-internal.primary-vault.svc.cluster.local",
+    "vault-4.vault-internal.primary-vault.svc.cluster.local"
   ]
 }
 
-variable "vault_common_name" {
+variable "auto_unseal_vault_common_name" {
   type        = string
-  description = "Common name for Vault certificate"
-  default     = "vault-dc1.hashibank.com"
+  description = "Common name for Auto Unseal Vault certificate"
+  default     = "auto-unseal-vault.hashibank.com"
+}
+
+variable "auto_unseal_vault_dns_names" {
+  type        = list(string)
+  description = "DNS names for Auto Unseal Vault certificate"
+  default = [
+    "auto-unseal-vault.hashibank.com",
+    "auto-unseal-vault-0.auto-unseal-vault-internal.auto-unseal-vault.svc.cluster.local",
+    "auto-unseal-vault-0.auto-unseal-vault.svc.cluster.local"
+  ]
 }
 
 variable "organization" {
@@ -54,7 +73,7 @@ variable "prometheus_helm_version" {
 
 # variable "prometheus_targets" {
 #   type    = list(string)
-#   default = ["vault-active.vault.svc.cluster.local:8200"]
+#   default = ["vault-active.primary-vault.svc.cluster.local:8200"]
 # }
 
 variable "grafana_helm_version" {
