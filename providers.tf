dbe4bfe28ca7f65d352a1503eab4dd395f3d93f7
Aaron Evans <aarone@outlook.com.au>
2025-03-11 14:12:12 +1100

    Add required providers for Terraform configuration

--- providers.tf
+++ providers.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_providers {
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 4"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "3.0.0-pre2"
+    }
+    onepassword = {
+      source = "1Password/onepassword"
+      version = "~> 2"
+    }
+  }
+}
+
 provider "tls" {
 }
 
@@ -11,4 +32,7 @@ provider "helm" {
     config_path    = var.kube_config_path
     config_context = var.kube_config_context
   }
+}
+
+provider "onepassword" {
 }
