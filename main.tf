4e8cbb189095f401d9a0966277338172ac477fa0
Aaron Evans <aarone@outlook.com.au>
2025-03-11 14:08:57 +1100

    Refactor Terraform modules to use local file data for CA certificates and update module sources

--- main.tf
+++ main.tf
@@ -1,37 +1,27 @@
-# main.tf
+# # main.tf
 
 module "namespaces" {
   source = "./modules/namespaces"
 }
 
-module "ca_cert" {
-  source                = "./modules/cert_creation"
-  common_name           = "vault-ca"
-  organization          = "HashiBank"
-  is_ca_certificate     = true
-  validity_period_hours = 87600
-  cert_file_name        = "vault-ca.crt"
-  key_file_name         = "vault-ca.key"
-  save_to_file          = true
-}
-
 module "ingress_nginx" {
   source            = "./modules/ingress_nginx"
   ingress_namespace = module.namespaces.nginx_namespace
-  ca_cert_pem       = module.ca_cert.cert_pem
+  ca_cert_pem       = data.local_file.root_ca_cert.content
 }
 
+# Generate a certificate for auto_unseal_vault using the Root CA
 module "auto_unseal_vault_cert" {
-  source                = "./modules/cert_creation"
-  ca_private_key_pem    = module.ca_cert.private_key_pem
-  ca_cert_pem           = module.ca_cert.cert_pem
+  source                = "github.com/hashi-demo-lab/terraform-cert-creation.git?ref=main"
+  ca_private_key_pem    = data.local_file.root_ca_key.content
+  ca_cert_pem           = data.local_file.root_ca_cert.content
   common_name           = var.auto_unseal_vault_common_name
   dns_names             = var.auto_unseal_vault_dns_names
   organization          = var.organization
   is_ca_certificate     = false
   validity_period_hours = 8760
-  cert_file_name        = "auto_unseal_vault.crt"
-  key_file_name         = "auto_unseal_vault.key"
+  cert_file_name        = "${path.root}/../certificates/auto_unseal_vault.crt"
+  key_file_name         = "${path.root}/../certificates/auto_unseal_vault.key"
   save_to_file          = true
 }
 
@@ -41,9 +31,9 @@ module "auto_unseal_vault" {
   vault_namespace = module.namespaces.auto_unseal_vault_namespace
 
   # Pass the certificate and key data from the auto_unseal_vault_cert module.
+  ca_cert_pem           = data.local_file.root_ca_cert.content
   vault_cert_pem        = module.auto_unseal_vault_cert.cert_pem
   vault_private_key_pem = module.auto_unseal_vault_cert.private_key_pem
-  ca_cert_pem           = module.ca_cert.cert_pem
 
   vault_dns_names   = var.auto_unseal_vault_dns_names
   vault_common_name = var.auto_unseal_vault_common_name
@@ -52,26 +42,25 @@ module "auto_unseal_vault" {
   # For auto‑unseal, deploy as a single‑node (HA disabled).
   vault_license                     = var.vault_license
   vault_release_name                = "auto-unseal-vault"
-  vault_helm                        = local.vault_helm_values
   vault_ha_enabled                  = false
   vault_initialization_script       = local.auto_unseal_vault_init_script
   auto_unseal_transit_config_script = local.auto_unseal_transit_config_script
   configure_seal                    = false
   vault_mode                        = "auto_unseal"
-  enable_service_registration       = false
+  enable_service_registration       = false #Non HA mode does not require service registration
 }
 
 module "primary_vault_cert" {
-  source                = "./modules/cert_creation"
-  ca_private_key_pem    = module.ca_cert.private_key_pem
-  ca_cert_pem           = module.ca_cert.cert_pem
+  source                = "github.com/hashi-demo-lab/terraform-cert-creation.git?ref=main"
+  ca_private_key_pem    = data.local_file.root_ca_key.content
+  ca_cert_pem           = data.local_file.root_ca_cert.content
   common_name           = var.primary_vault_common_name
   dns_names             = var.primary_vault_dns_names
   organization          = var.organization
   is_ca_certificate     = false
   validity_period_hours = 8760
-  cert_file_name        = "vault.crt"
-  key_file_name         = "vault.key"
+  cert_file_name        = "${path.root}/../certificates/vault.crt"
+  key_file_name         = "${path.root}/../certificates/vault.key"
   save_to_file          = true
 }
 
@@ -82,24 +71,34 @@ module "primary_vault" {
   vault_namespace = module.namespaces.primary_vault_namespace
 
   # Pass the certificate and key data from the primary_vault_cert module
+  ca_cert_pem           = data.local_file.root_ca_cert.content
   vault_cert_pem        = module.primary_vault_cert.cert_pem
   vault_private_key_pem = module.primary_vault_cert.private_key_pem
-  ca_cert_pem           = module.ca_cert.cert_pem
 
   organization      = var.organization
   vault_dns_names   = var.primary_vault_dns_names
   vault_common_name = var.primary_vault_common_name
 
   vault_license               = var.vault_license
-  vault_helm                  = local.vault_helm_values
   vso_helm                    = local.vso_helm_values
   vault_initialization_script = local.primary_vault_init_script
   configure_seal              = true
   vault_mode                  = "primary"
   enable_service_registration = true
-  aws_access_key_id           = var.aws_access_key_id
-  aws_secret_access_key       = var.aws_secret_access_key
-  aws_session_token           = var.aws_session_token
+  aws_credentials             = var.aws_credentials
+}
+
+data "onepassword_vault" "vault_lab" {
+  name = "Vault Lab"
+}
+
+resource "onepassword_item" "vault_root_token" {
+  vault    = data.onepassword_vault.vault_lab.uuid
+  title    = "HashiCorp Vault Lab Root Token"
+  category = "login"
+  url      = "https://vault.hashibank.com/"
+  username = "token"
+  password = module.primary_vault.root_token
 }
 
 # module "monitoring" {
@@ -125,15 +124,15 @@ module "primary_vault" {
 #   vault_root_token         = local.decoded_root_token
 # }
 
-# # module "neo4j" {
-# #   source = "./modules/neo4j"
+# module "neo4j" {
+#   source = "./modules/neo4j"
 
-# #   neo4j_namespace   = module.namespaces.neo4j_namespace
-# #   helm_release_name = "neo4j"
-# #   helm_repository   = "https://helm.neo4j.com/neo4j"
-# #   helm_chart_name   = "neo4j"
-# #   helm_values       = local.neo4j_helm_values
-# # }
+#   neo4j_namespace   = module.namespaces.neo4j_namespace
+#   helm_release_name = "neo4j"
+#   helm_repository   = "https://helm.neo4j.com/neo4j"
+#   helm_chart_name   = "neo4j"
+#   helm_values       = local.neo4j_helm_values
+# }
 
 
 # module "ldap_cert" {
@@ -184,3 +183,10 @@ module "primary_vault" {
 # # module "cert-manager" {
 # #   source = "./modules/cert-manager"
 # # }
+
+module "hostname_service" {
+  source = "./modules/hostnaming-service"
+
+  namespace = module.namespaces.hostnaming-service_namespace
+  hostname_manifest = local.hostname_manifest
+}
