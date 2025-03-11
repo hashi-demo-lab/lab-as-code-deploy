e56a379478faa23c58adffbae6fcde0727844ad7
Aaron Evans <aarone@outlook.com.au>
2025-03-11 14:09:12 +1100

    Remove unused CA certificate outputs from Vault instructions

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,10 @@
 output "instructions" {
   value = {
     "vault" = {
-      "address"         = "https://${var.primary_vault_common_name}:443"
-      "ca_cert"         = module.ca_cert.cert_file_path
-      "encoded_ca_cert" = base64encode(module.ca_cert.cert_pem)
+      "address" = "https://${var.primary_vault_common_name}:443"
+      #"ca_cert"         = module.ca_cert.cert_file_path
+      #"encoded_ca_cert" = base64encode(module.ca_cert.cert_pem)
       "commands" = {
-        "trust_cert"       = "sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ${path.root}/${module.ca_cert.cert_file_path}"
         "root_token"       = "kubectl config use-context docker-desktop; kubectl get secret vault-init-credentials -n primary-vault -o jsonpath={.data.root-token} | base64 --decode"
         "update_etc_hosts" = "echo -e 127.0.0.1 ${var.primary_vault_common_name} | sudo tee -a /etc/hosts > /dev/null"
       }
