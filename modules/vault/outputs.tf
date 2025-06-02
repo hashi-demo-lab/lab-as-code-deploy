0470bbb6c1a55d5a42a6dce92cdf664c1bcb03ae
Aaron Evans <aarone@outlook.com.au>
2025-06-02 12:15:40 +1000

    Add output for Vault unseal key in outputs.tf

--- modules/vault/outputs.tf
+++ modules/vault/outputs.tf
@@ -2,3 +2,9 @@ output "root_token" {
   description = "The decoded Vault root token from the vault-init-credentials secret"
   value       = try(nonsensitive(data.kubernetes_secret.vault_init_credentials.data["root-token"]), "")
 }
+
+#unseal key if it exists
+output "unseal_key" {
+  description = "The decoded Vault unseal key from the vault-init-credentials secret"
+  value       = try(nonsensitive(data.kubernetes_secret.vault_init_credentials.data["unseal-key"]), "")
+}
